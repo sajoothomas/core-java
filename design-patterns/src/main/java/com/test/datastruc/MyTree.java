1ceca4d0ecb8c87a244684a72cfc2f8c94e06937
Sajoo Thomas <sajoo.thomas@gmail.com>
2018-05-26 17:19:30 +0530

    Tree implemenation

---
+++ design-patterns/src/main/java/com/test/datastruc/MyTree.java
@@ -0,0 +1,5 @@
+package com.test.datastruc;
+
+public class MyTree {
+
+}
---
+++ design-patterns/src/test/java/com/test/datastruc/MyTreeTest.java
@@ -0,0 +1,14 @@
+package com.test.datastruc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyTreeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
